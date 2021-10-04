1054d6de0cba28ba895b1d16d273d2f74da4266a
zyhou <richard.maxime9@gmail.com>
2021-10-04 19:20:44 +0200

    fix rules

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
